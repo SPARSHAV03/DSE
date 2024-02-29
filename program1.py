7d1e0a15eb407dff1b2686b4ff8afac4423bb886
SPARSHAV03 <sparshav2003@gmail>
2024-02-29 19:45:16 +0530

    commit

--- program1.py
+++ program1.py
@@ -1,18 +1,26 @@
-import os
-print("Operating system:", os.name)
-print("Working directory:",os.getcwd())
-files_and_dirs=os.listdir()
-files=[]
-directories=[]
-for item in files_and_dirs:
-  if os.path.isfile(item):
-    files.append(item)
-  elif os.path.isdir(item):
-    directories.append(item)
-
-  print("\n Files:")
-  for file in files:
-    print(file)
-  print("\n Directories:")
-  for directory in directories:
-    print(directory)
+import matplotlib.pyplot as plt
+import numpy as np
+# Generate some data for plotting
+x = np.linspace(0, 10, 100)
+y = np.sin(x)
+plt.figure()
+plt.plot(x,y)
+plt.title("Line Chart")
+categories=['A','B','C','D']
+values=[20,35,30,25]
+plt.figure()
+plt.bar(categories,values)
+plt.title("Bar Chart")
+x=np.random.randn(100)
+y=np.random.randn(100)
+colors=np.random.rand(100)
+sizes=100*np.random.rand(100)
+plt.figure()
+plt.scatter(x,y,c=colors, s=sizes, alpha=0.5)
+plt.title("Scatter Plot")
+sizes = [30, 20, 25, 15, 10]
+labels = ['A', 'B', 'C', 'D', 'E']
+plt.figure()
+plt.pie(sizes, labels=labels, autopct="%1.1f%%")
+plt.title("Pie Chart")
+plt.show()
